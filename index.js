71670fe48c9e225aa59b11098ff4bd2d4911580a
shahstudios <sshahcode@gmail.com>
2021-07-04 13:21:11 -0400

    refactor code

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const grid = document.querySelector('.grid')
-const startBtn = document.getElementById('start')
+const startButton = document.getElementById('start')
 const score = document.getElementById('score')
 
 let squares = []
-let currentSnake = [2,1,0]
+let currentSnake = [2, 1, 0]
 let direction = 1
-let width = 10
+const width = 10
 
 function createGrid() {
-    for (let i=0; i<100; i++) {
+    for (let i = 0; i < 100; i++) {
         const square = document.createElement('div')
         square.classList.add('square')
         grid.appendChild(square)
@@ -19,15 +19,35 @@ createGrid()
 
 currentSnake.forEach(index => squares[index].classList.add('snake'))
 
+function createGrid() {
+    for (let i = 0; i < width * width; i++) {
+        const square = document.createElement('div')
+        grid.appendChild(square)
+        squares.push(square)
+    }
+}
+createGrid()
+
+currentSnake.forEach(index => squares[index].classList.add('snake'))
+
 function move() {
-    const snakeTail = currentSnake.pop()
-    squares[snakeTail].classList.remove('snake')
+    if (
+        (currentSnake[0] + width >= width * width && direction === width) || //if snake has hit bottom
+        (currentSnake[0] % width === width - 1 && direction === 1) || //if snake has hit right wall
+        (currentSnake[0] % width === 0 && direction === -1) || //if snake has hit left wall
+        (currentSnake[0] - width < 0 && direction === -width) || //if snake has hit top
+        squares[currentSnake[0] + direction].classList.contains('snake')
+    )
+        return clearInterval(timerId)
+
+    const tail = currentSnake.pop()
+    squares[tail].classList.remove('snake')
     currentSnake.unshift(currentSnake[0] + direction)
     squares[currentSnake[0]].classList.add('snake')
 }
 move()
 
-let timerId = setInterval(move, 150)
+let timerId = setInterval(move, 1000)
 
 function control(e) {
     if (e.keyCode === 39) {
@@ -41,7 +61,7 @@ function control(e) {
         direction = -1
     } else if (e.keyCode === 40) {
         console.log('down pressed')
-        direction = +width;
+        direction = +width
     }
 }
 document.addEventListener('keyup', control)
